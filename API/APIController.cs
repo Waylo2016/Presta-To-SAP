7802e990a1fa50a58812bf4e3e95e0f345863fad
Stijn Wellink <wellinkstijn@gmail.com>
2025-11-11 12:18:34 +0100

    added first bit of API logic
    added API logic, can easily call the different API links

---
+++ API/APIController.cs
@@ -0,0 +1,19 @@
+﻿namespace PrestaToSap.API;
+
+public class APIController
+{
+    // get API links from appsettins.json
+    public IConfigurationRoot Config()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        return config;
+    }
+    
+    public List<string> GetAPILinks(IConfigurationRoot config)
+    {
+        return config.GetSection("API URLs").Get<List<string>>();
+    }
+}
---
+++ API/CallAPI.cs
@@ -0,0 +1,19 @@
+﻿namespace PrestaToSap.API;
+
+public class CallAPI
+{
+    
+    public void CallOrders()
+    {
+        APIController apiController = new();
+        var config = apiController.Config();
+        List<string> apiLinks = apiController.GetAPILinks(config);
+
+        foreach (var apiLink in apiLinks)
+        {
+            
+        }
+        
+    }
+}
+
